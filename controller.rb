88cf9e75097bac7ca32c9fccf3758388ea73c04d
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-01 15:42:30 +0100

    created layouts for home and rules pages

--- controller.rb
+++ controller.rb
@@ -2,7 +2,16 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('models/game')
 
+get '/' do
+  erb(:home)
+end
+
+get '/rules' do
+  erb(:rules)
+end
+
 get '/:player1/:player2' do |player1, player2|
   game = Game.new(player1,player2)
-  return game.play()
+  @outcome = game.play()
+  erb(:result)
 end
